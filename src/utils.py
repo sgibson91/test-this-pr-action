1dc44570c17e66a1b47055f71ee5833943670a5a
Sarah Gibson <drsarahlgibson@gmail.com>
2021-07-03 12:00:04 +0100

    ✨ Add helper functions to run shell commands

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+"""
+Helper functions
+"""
+import subprocess
+
+
+def run_cmd(cmd: list) -> dict:
+    """Use Popen to run a bash command in a sub-shell
+
+    Args:
+        cmd (list): The bash command to run
+
+    Returns:
+        dict: The output of the command, including status code and error
+              messages
+    """
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    msgs = proc.communicate()
+
+    result = {
+        "returncode": proc.returncode,
+        "output": msgs[0].decode(encoding=("utf-8")).strip("\n"),
+        "err_msg": msgs[1].decode(encoding=("utf-8")).strip("\n"),
+    }
+
+    return result
