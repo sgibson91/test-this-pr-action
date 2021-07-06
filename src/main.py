b723a51d1f07d073dab3bceed692573460ef8f2b
Sarah Gibson <drsarahlgibson@gmail.com>
2021-07-06 13:42:36 +0100

    ♻️ Major refactor
    - Use GhApi instead of requests
    - Reduce number of environment variables required, retrieve others with
      GhApi calls
    - No longer create Pull Request, just push to a branch

--- src/main.py
+++ src/main.py
@@ -1,32 +1,24 @@
 import os
-from utils import post_request, run_cmd
+import json
+from utils import run_cmd
+from ghapi.all import GhApi
+from ghapi.actions import github_token
 
 # Set required environment variables
 REPOSITORY = (
     os.environ["INPUT_REPOSITORY"] if "INPUT_REPOSITORY" in os.environ else None
 )
-FORK_OWNER = (
-    os.environ["INPUT_FORK_OWNER"] if "INPUT_FORK_OWNER" in os.environ else None
-)
-PR_NUMBER = os.environ["INPUT_PR_NUMBER"] if "INPUT_PR_NUMBER" in os.environ else None
-PR_BRANCH_NAME = (
-    os.environ["INPUT_PR_BRANCH_NAME"] if "InPUT_PR_BRANCH_NAME" in os.environ else None
+GITHUB_ISSUE_CONTEXT = (
+    json.loads(os.environ["INPUT_GITHUB-ISSUE-CONTEXT"]) if "INPUT_GITHUB-ISSUE-CONTEXT" in os.environ else None
 )
 GITHUB_TOKEN = (
-    os.environ["INPUT_GITHUB_TOKEN"] if "INPUT_GITHUB_TOKEN" in os.environ else None
-)
-
-# Set optional environment variables
-APPLY_LABELS = (
-    os.environ["INPUT_APPLY_LABELS"] if "INPUT_APPLY_LABELS" in os.environ else None
+    github_token() if "INPUT_GITHUB-TOKEN" not in os.environ else os.environ["INPUT_GITHUB-TOKEN"]
 )
 
 # Check required environment variables are set
 REQUIRED_ENV_VARS = {
     "REPOSITORY": REPOSITORY,
-    "FORK_OWNER": FORK_OWNER,
-    "PR_NUMBER": PR_NUMBER,
-    "PR_BRANCH_NAME": PR_BRANCH_NAME,
+    "GITHUB_ISSUE_CONTEXT": GITHUB_ISSUE_CONTEXT,
     "GITHUB_TOKEN": GITHUB_TOKEN,
 }
 
@@ -34,7 +26,23 @@ for VARNAME, VAR in REQUIRED_ENV_VARS.items():
     if VAR is None:
         raise ValueError(f"{VARNAME} must be set")
 
-PROJECT_NAME = REPOSITORY.split("/")[-1]
+# Initialise GhApi
+api = GhApi(token=GITHUB_TOKEN)
+
+# Set repository name
+REPO_NAME = REPOSITORY.split("/")[-1]
+
+# Set Pull Request number
+PR_NUMBER = GITHUB_ISSUE_CONTEXT["number"]
+
+# Get Pull Request info
+pr_info = api.pulls.get(REPOSITORY.split("/")[0], REPOSITORY.split("/")[-1], PR_NUMBER)
+
+# Set fork owner
+FORK_OWNER = pr_info.head.user.login
+
+# Set Pull Request branch name
+PR_BRANCH_NAME = pr_info.head.ref
 
 # Clone the parent repo
 _ = run_cmd(
@@ -46,7 +54,7 @@ _ = run_cmd(
 )
 
 # Change working directory
-os.chdir(PROJECT_NAME)
+os.chdir(REPO_NAME)
 
 # Add fork as remote
 _ = run_cmd(
@@ -55,7 +63,7 @@ _ = run_cmd(
         "remote",
         "add",
         "fork",
-        f"https://github.com/{FORK_OWNER}/{PROJECT_NAME}.git",
+        f"https://github.com/{FORK_OWNER}/{REPO_NAME}.git",
     ]
 )
 
@@ -97,47 +105,5 @@ _ = run_cmd(
     ]
 )
 
-# Get the default branch of the parent repo
-# to set as base branch of Pull Request
-result = run_cmd(
-    [
-        "git",
-        "symbolic-ref",
-        "refs/remotes/origin/HEAD",
-    ]
-)
-base_branch = result["output"].split("/")[-1]
-
-# Create Pull Request template
-pr = {
-    "title": f"Testing PR #{PR_NUMBER}",
-    "body": f"This PR is a copy of PR #{PR_NUMBER} which came from a fork. Tests that require secrets can now be run on this PR.",
-    "base": base_branch,
-    "head": f"test-this-pr/{PR_NUMBER}",
-}
-
-# Create the Pull Request
-resp = post_request(
-    f"https://api.github.com/repos/{REPOSITORY}/pulls",
-    headers={"Authorization": f"token {GITHUB_TOKEN}"},
-    json=pr,
-    return_json=True,
-)
-
-PR_URL = resp["html_url"]
-PR_URL_API = resp["issue_url"]
-
-# Add labels to the new PR
-if APPLY_LABELS is not None:
-    post_request(
-        PR_URL_API + "/labels",
-        headers={"Authorization": f"token {GITHUB_TOKEN}"},
-        json={"labels": APPLY_LABELS.split(" ")},
-    )
-
 # Add comment to the old PR
-post_request(
-    f"https://api.github.com/repos/{REPOSITORY}/issues/{PR_NUMBER}/comments",
-    headers={"Authorization": f"token {GITHUB_TOKEN}"},
-    json={"body": f"This Pull Request is now being tested in {PR_URL}"},
-)
+api.issues.create_comment(REPOSITORY.split("/")[0], REPOSITORY.split("/")[-1], PR_NUMBER, body=f"This Pull Request is now being tested on the following branch: https://github.com/binderhub-test-org/pr-test/tree/test-this-pr/{PR_NUMBER}")
