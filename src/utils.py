1c82e1559273b949153d2d875f83bbb7b70d038d
Sarah Gibson <drsarahlgibson@gmail.com>
2021-07-14 17:47:34 +0100

    ✨ Add wrapper dectorator to log func entry and exit
    Make sure this decorator preserves privacy by using regex to strip
    ACCESS_TOKEN from appropriate calls

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,40 @@
 """
 Helper functions
 """
+import re
+import functools
 import subprocess
+from loguru import logger
+from copy import deepcopy
 
 
+def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
+
+    def wrapper(func):
+        name = func.__name__
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger_ = logger.opt(depth=1)
+
+            # Prevent the logger from leaking ACCESS_TOKEN using regex to select
+            # all characters between 'https://' and ':x-oauth-basic' in `git clone`
+            # and `git remote set-url` commands
+            clean_args = deepcopy(args)
+            if ("clone" in clean_args[0]) or ("set-url" in clean_args[0]):
+                clean_args[0][-1] = re.sub(r"(?<=\/\/)(.*?)(?=:)", "***", clean_args[0][-1])
+
+            if entry:
+                logger_.log(level, "Entering '{}' (args={}, kwargs{})", name, clean_args, kwargs)
+            result = func(*args, **kwargs)
+
+            if exit:
+                logger_.log(level, "Exiting '{}' (result={})", name, result)
+            return result
+
+        return wrapped
+
+    return wrapper
 def run_cmd(cmd: list) -> dict:
     """Use Popen to run a bash command in a sub-shell
 
