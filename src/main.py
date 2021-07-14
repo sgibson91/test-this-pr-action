508118b29c870b27eda7ff8c1292e6e36391ec58
Sarah Gibson <drsarahlgibson@gmail.com>
2021-07-14 17:45:30 +0100

    🔊 Add logging statements to main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from utils import run_cmd
+from loguru import logger
 from ghapi.all import GhApi
 from ghapi.actions import github_token
 
@@ -21,14 +22,18 @@ REQUIRED_ENV_VARS = {
     "PR_NUMBER": PR_NUMBER,
 }
 
+logger.info("Checking environment variables...")
 for VARNAME, VAR in REQUIRED_ENV_VARS.items():
     if VAR is None:
+        logger.error("Unset environment variable!")
         raise ValueError(f"{VARNAME} must be set")
+logger.info("Environment variables all good!")
 
 # Set repository name
 REPO_NAME = REPOSITORY.split("/")[-1]
 
 # Clone the parent repo
+logger.info(f"Cloning parent repository: {REPOSITORY}")
 _ = run_cmd(
     [
         "git",
@@ -38,14 +43,17 @@ _ = run_cmd(
 )
 
 # Change working directory
+logger.info(f"Changing into dir: {REPO_NAME}")
 os.chdir(REPO_NAME)
 
 # Fetch the existing merge ref and create a new local branch
+logger.info(f"Fetching details for PR #{PR_NUMBER} and adding to branch test-this-pr/{PR_NUMBER}")
 _ = run_cmd(
     ["git", "fetch", "origin", f"pull/{PR_NUMBER}/merge:test-this-pr/{PR_NUMBER}"]
 )
 
 # Push new branch to parent repo
+logger.info(f"Force pushing branch: test-this-pr/{PR_NUMBER}")
 _ = run_cmd(
     [
         "git",
@@ -60,6 +68,7 @@ _ = run_cmd(
 api = GhApi(token=ACCESS_TOKEN)
 
 # Add comment to the old PR
+logger.info(f"Adding comment to PR #{PR_NUMBER}")
 api.issues.create_comment(
     REPOSITORY.split("/")[0],
     REPOSITORY.split("/")[-1],
