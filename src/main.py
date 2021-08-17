c8e0bb4a195bc7f7d901856cbcb890b66e015327
Sarah Gibson <drsarahlgibson@gmail.com>
2021-08-17 18:20:34 +0100

    ♻️ Refactor main.py to make HTTP requests, not git cmd line

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 import os
-from utils import run_cmd
-from loguru import logger
-from ghapi.all import GhApi
-from ghapi.actions import github_token
+from utils import get_request, post_request
+
 
 # Set required environment variables
 ACCESS_TOKEN = (
-    github_token()
-    if "INPUT_ACCESS_TOKEN" not in os.environ
-    else os.environ["INPUT_ACCESS_TOKEN"]
+    os.environ["INPUT_ACCESS_TOKEN"] if "INPUT_ACCESS_TOKEN" in os.environ else None
 )
 REPOSITORY = (
     os.environ["INPUT_REPOSITORY"] if "INPUT_REPOSITORY" in os.environ else None
@@ -22,72 +18,33 @@ REQUIRED_ENV_VARS = {
     "INPUT_PR_NUMBER": PR_NUMBER,
 }
 
-logger.info("Checking environment variables...")
 for VARNAME, VAR in REQUIRED_ENV_VARS.items():
     if VAR is None:
-        logger.error("Unset environment variable!")
-        raise ValueError(f"{VARNAME} must be set")
-logger.info("Environment variables all good!")
+        raise ValueError(f"{VARNAME} must be set!")
 
-# Set repository name
-REPO_NAME = REPOSITORY.split("/")[-1]
+# API URL and default header
+API_URL = "https://api.github.com"
+HEADER = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {ACCESS_TOKEN}"
+}
 
-# Clone the parent repo
-logger.info(f"Cloning parent repository: {REPOSITORY}")
-_ = run_cmd(
-    [
-        "git",
-        "clone",
-        f"https://{ACCESS_TOKEN}:x-oauth-basic@github.com/{REPOSITORY}.git",
-    ]
-)
+# Fetch merge ref
+merge_ref_url = f"{API_URL}/repos/{REPOSITORY}/git/ref/pull/{PR_NUMBER}/merge"
+resp = get_request(merge_ref_url, headers=HEADER, output='json')
 
-# Change working directory
-logger.info(f"Changing into dir: {REPO_NAME}")
-os.chdir(REPO_NAME)
+# Create new branch ref
+new_branch_ref_url = f"{API_URL}/repos/{REPOSITORY}/git/refs"
+body = {
+    "ref": f"refs/heads/test-this-pr/{PR_NUMBER}",
+    "sha": resp["object"]["sha"]
+}
+post_request(new_branch_ref_url, headers=HEADER, json=body)
 
-# Set remote URL to use credentials
-logger.info("Updating remote to use credentials when pushing...")
-_ = run_cmd(
-    [
-        "git",
-        "remote",
-        "set-url",
-        "origin",
-        f"https://{ACCESS_TOKEN}:x-oauth-basic@github.com/{REPOSITORY}.git",
-    ]
-)
-
-# Fetch the existing merge ref and create a new local branch
-logger.info(
-    f"Fetching details for PR #{PR_NUMBER} and adding to branch test-this-pr/{PR_NUMBER}"
-)
-_ = run_cmd(
-    ["git", "fetch", "origin", f"pull/{PR_NUMBER}/merge:test-this-pr/{PR_NUMBER}"]
-)
-
-# Push new branch to parent repo
-logger.info(f"Force pushing branch: test-this-pr/{PR_NUMBER}")
-_ = run_cmd(
-    [
-        "git",
-        "push",
-        "--force",
-        "origin",
-        f"test-this-pr/{PR_NUMBER}",
-    ]
-)
-
-# Initialise GhApi
-api = GhApi(token=ACCESS_TOKEN)
-
-# Add comment to the old PR
-logger.info(f"Adding comment to PR #{PR_NUMBER}")
-api.issues.create_comment(
-    REPOSITORY.split("/")[0],
-    REPOSITORY.split("/")[-1],
-    PR_NUMBER,
-    body=f"""
+# Create a comment on the PR
+issue_url = f"{API_URL}/repos/{REPOSITORY}/issues/{PR_NUMBER}/comments"
+body = {
+    "body": f"""
 This Pull Request is now being tested :tada: See the test progress in [GitHub Actions](https://github.com/{REPOSITORY}/actions?query=branch%3Atest-this-pr%2F{PR_NUMBER}).
-""",
-)
+"""}
+post_request(issue_url, headers=HEADER, json=body)
